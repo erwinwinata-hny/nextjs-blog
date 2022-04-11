a4eddc67a65b35fbb6a2872d97c6dd5542b303a6
Fairs <erwinwinata@gmail.com>
2022-04-11 21:53:28 +0700

    Dynamic Routes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+import Date from '../components/date'
 import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
+import utilStyles from '../styles/utils.module.css'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
@@ -18,7 +21,7 @@ export default function Home({ allPostsData }) {
       <Head>
         <title>{siteTitle}</title>
       </Head>
-      
+
       <section className={utilStyles.headingMd}>
         <p>I'm just a beginner developer who's trying to learn about Next.js</p>
         <p>
@@ -32,11 +35,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
